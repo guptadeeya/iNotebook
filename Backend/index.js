1251b74a1806fb43fb2e03842ff2aa4af4cac52a
Deeya <guptadeeya12@gmail.com>
2023-03-31 23:20:47 +0530

    test commit 3

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-// Available Routes test
+// Available Routes
 app.use('/api/auth', require('./routes/auth'))
 // app.use('/api/notes', require('./routes/notes'))
 
