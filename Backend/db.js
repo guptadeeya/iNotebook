3816d71f25edac314bfae092756ff6fb482ae792
Deeya <guptadeeya12@gmail.com>
2023-04-03 16:57:24 +0530

    Storing Data in db

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 // .then( () => console.log("connection successful"))
 // .catch((err) => console.log(err));
 
-const mongoURI = "mongodb://localhost:27017"
+// const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb+srv://Deeya:Frstdata@cluster0.ta9amdj.mongodb.net/test";
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
         // if(err) console.log(`unable to connect to the server: ${err}`);
@@ -13,5 +14,4 @@ const connectToMongo = () =>{
         console.log("Connected to mongoose successfully");
     })
 }
-
 module.exports = connectToMongo;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express')
-
+const User = require('../models/User')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    obj = {
-        a: "thios",
-        number: 34
-    }
-    res.json(obj)
+// create a user using: POST "/api/auth/"   Doesn't require auth
+
+router.post('/', (req, res)=>{
+    // obj = {
+    //     a: "thios",
+    //     number: 34
+    // }
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+
+    res.send(req.body);
+    // res.json(obj)
 })
 
 module.exports = router
+
+// C:\Users\pc\AppData\Roaming\npm
