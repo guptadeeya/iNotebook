d9966a9204baaa37bb3ea18bbbdf18f99dbab3bd
Deeya <guptadeeya12@gmail.com>
2023-04-04 22:24:30 +0530

    Managing Requests(thunderclient)

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,13 +3,12 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-// create a user using: POST "/api/auth/"   Doesn't require auth
+// create a user using: POST "/api/auth/createuser"   No login required
 
-router.post('/',[body('email').isEmail(),
+router.post('/createuser',[body('email').isEmail(),
      body('name', 'Enter a valid name').isLength({ min: 3 }),
      body('password').isLength({ min: 5 })
-    ],
-    (req, res)=>{
+    ], async(req, res)=>{
     // obj = {
     //     a: "thios",
     //     number: 34
@@ -24,16 +23,32 @@ router.post('/',[body('email').isEmail(),
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
+//check whether the user with this email already exists
+
+// try{
+
+// }
+// catch{
+
+// }
+
+let user =  await User.findOne({email: req.body.email});
+// console.log(user);
+if(user){
+  return res.status(400).json({error: "Sorry a user with email already exists"});
+}
+    user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user));
+      })
+      // .then(user => res.json(user));
 
     // res.send(req.body);
     // res.json(obj)
-})
 
+    res.json(user)
+})
 module.exports = router
 
 // C:\Users\pc\AppData\Roaming\npm
