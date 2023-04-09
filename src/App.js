dc7b238eb71be85e96e749fb34e5dc00d2ed49a4
Deeya <guptadeeya12@gmail.com>
2023-04-09 11:54:18 +0530

    Adding Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-    <h1>This is iNotebook</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    {/* <Home/> */}
+
+    <Routes>
+          <Route path="/" element={<Home />}> </Route>
+          <Route path="/About" element={<About />}> </Route>
+        </Routes>
+        </Router>
+    </>
   );
 }
 
